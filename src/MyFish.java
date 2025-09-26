bd670ec487772775c6606c4345b62006fa5a2a37
AndoPrice <andolucprice@gmail.com>
2025-09-26 14:47:15 +0200

    1

---
+++ src/MyFish.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class MyFish extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("MyFish");
+    }
+
+    public void settings(){
+        fullScreen();
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+
+    }
+
+}
